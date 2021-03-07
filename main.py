8ca95ef447d1f87338ea2952ec0b39c7489a5dde
Yehonatan <yehonatan.goldfarb@gmail.com>
2021-03-08 01:50:05 +0200

    Uploaded server and links file

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+
+def parse_links_file(filename):
+    result = []  # Dictionary mapping from address name to details
+    with open(filename, 'r', encoding='utf8') as f:
+        data = f.read()
+        lines = data.splitlines()
+        for line in lines:
+            fields = line.split(',')
+            result.append(fields)
+
+    return result
+
+
+def toHTMLFormat(element):
+    str1 = "<a href=\"".strip() + element[1].strip() + "\">"
+    str1 += element[0]
+    str1 += "</a><br>"
+    return str1
+
+
+@app.route("/request")
+def request():
+    random_list = random.sample(links_list, 3)
+    result = "<div dir=\"rtl\">\n"
+    for element in random_list:
+        result += toHTMLFormat(element)
+    result += "</div>"
+    return result
+
+
+if __name__ == "__main__":
+    links_list = parse_links_file("links&q.txt")
+    length = len(links_list)
+    app.run(debug=True)
