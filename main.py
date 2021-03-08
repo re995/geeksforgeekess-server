1106185e29a733cfee76f7dff00fedccf209c99b
Ron Even <ron.even@watchful.ai>
2021-03-08 11:32:15 +0200

    Allowed requests from non-localhost and made links open in new tab

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 import random
 
 app = Flask(__name__)
-
+CORS(app)
 
 def parse_links_file(filename):
     result = []  # Dictionary mapping from address name to details
@@ -17,7 +18,7 @@ def parse_links_file(filename):
 
 
 def toHTMLFormat(element):
-    str1 = "<a href=\"".strip() + element[1].strip() + "\">"
+    str1 = "<a href=\"".strip() + element[1].strip() + "\" target='_blank'>"
     str1 += element[0]
     str1 += "</a><br>"
     return str1
@@ -26,7 +27,7 @@ def toHTMLFormat(element):
 @app.route("/request")
 def request():
     random_list = random.sample(links_list, 3)
-    result = "<div dir=\"rtl\">\n"
+    result = "<div dir='rtl'>\n"
     for element in random_list:
         result += toHTMLFormat(element)
     result += "</div>"
